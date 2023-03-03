e29a4a68e9941a28c251019ba15b5e9ddc3fcf93
Jeff Burke <jburke0519@gmail.com>
2023-03-03 13:45:25 -0500

    Changed exit command

--- client.py
+++ client.py
@@ -6,10 +6,12 @@ PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 client.sendall("Client Connected".encode())
-while True:
+i = 0
+while i < 1:
     data = client.recv(1024)
     if data == "quit":
         break
     path = os.path
     client.sendall(f"{path}:".encode())
+    i += 1
 client.close()
