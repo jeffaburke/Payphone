bbfb4e43f8de803d592d8ba0a836ee65f99b5159
Jeff Burke <jburke0519@gmail.com>
2023-03-22 19:42:27 -0400

    ICMP Server packet crafting start

---
+++ ICMPServer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# start the program
+# ask a user for an input command to the source
+# send the command over in some encoded way
+#   craft the packet
+# wait for new command
+
+from scapy.all import *
+from scapy.layers.inet import IP, ICMP
+
+target = "0.0.0.0"
+
+
+def makeICMPPacket(command):
+    global target
+    toBeSent = IP(dst=target) / ICMP(type=0) / command
+    send(toBeSent)
+
+
+# start program
+def main() -> None:
+    from sys import argv
+
+    global target
+    target = argv[1]
+    while 1:
+        command_input = str(input("Command input >> "))
+        makeICMPPacket(command_input)
+
+
+if __name__ == "__main__":
+    main()
