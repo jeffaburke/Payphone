50d71c9de91f4ae5579de5bf595c78e2e16d74aa
Jeff Burke <jburke0519@gmail.com>
2023-03-22 20:00:44 -0400

    Minor little error

--- ICMPClient.py
+++ ICMPClient.py
@@ -4,20 +4,19 @@
 # TODO Later: implement DNS packet transfering with scapy, packet crafting
 # TODO Later: implement some jitter for client/beacon traffic sleeping assuming we have a sendback MAYBE
 # Look into platform as a service things (for the waaay future) for getting rnadom domains it would point to a cloud machine (university envir could be on the cloud) with firewall rules then the c2 server is behind the cloud machine
-from scapy.all import AsyncSniffer
+from scapy.all import AsyncSniffer, Packet
 import subprocess
 from time import sleep
 
 
-def sniffHandle(packet) -> None:
+def sniffHandle(packet: Packet) -> None:
     """Gets called every time a packet from the sniffer is sent, as of right now it prints the payload of the packet"""
-    payload = packet.payload
-    print(payload.split("@")[1])
+    print(packet[3].load.decode())
 
 
 def sniffer() -> None:
     """Starts an asynchronus sniffer then allows for the program to be interrupted"""
-    sniffer = AsyncSniffer(filter="icmp[icmptype] = icmp-echoreply", prn=sniffHandle)
+    sniffer = AsyncSniffer(filter="icmp", prn=sniffHandle)
 
     print("[*] Start sniffing...")
     sniffer.start()
